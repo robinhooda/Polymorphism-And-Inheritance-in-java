cc39bc28c62117b051a5f95ed18358d6d2edb4d8
Robin Hooda <robinhooda66@gmail.com>
2020-03-13 03:48:34 +0530

    Delete SweetCreatorTest.java

--- src/main/java/SweetCreatorTest.java
+++
@@ -1,9 +0,0 @@
-import java.util.Arrays;
-
-public class SweetCreatorTest {
-    public static void main(String... args) {
-        SweetCreator sweetCreator = new SweetCreator(Arrays.asList(new CakeProducer(),
-                new ChocolateProducer(), new CookieProducer()));
-        sweetCreator.createSweets();
-    }
-}
